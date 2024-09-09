5a8b0ab41d754026e51e4e980da552ff88508e87
Alicia Benavente <aliciabenaventegarcia@gmail.com>
2024-09-09 17:27:41 +0000

    deleted fav_Stored and cleaned code

--- src/models.py
+++ src/models.py
@@ -9,8 +9,6 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     creation_date = Column(Integer, nullable=False)
@@ -18,62 +16,22 @@ class User(Base):
 
 class Login(Base):
     __tablename__ = 'login'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     planets_id = Column(Integer, ForeignKey('planets.id'))
     characters_id = Column(Integer, ForeignKey('characters.id'))
     person = relationship(User)
-    # planet = relationship(Planets)
-    # character = relationship(Characters)
 
-class Fav_stored(Base):
-    __tablename__ = 'fav_stored'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    planets_id = Column(Integer, ForeignKey('planets.id'))
-    characters_id = Column(Integer, ForeignKey('characters.id'))
-    person = relationship(User)
-    # planet = relationship(Planets)
-    # character = relationship(Characters)
 
 class Planets(Base):
     __tablename__ = 'planets'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     
 class Characters(Base):
     __tablename__ = 'characters'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-
-
-
-
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
 
     def to_dict(self):
         return {}
